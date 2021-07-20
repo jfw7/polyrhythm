efc50bba4a573caf7780eee0bf19282029d1470c
jfw7 <jonathan@jonathanwallmusic.com>
2021-07-20 13:18:35 +0000

    Deploying to gh-pages from  @ 68a37e488da85b9202683827ac26782b4aaa9928 🚀

--- bundle.js
+++ bundle.js
@@ -13203,17 +13203,16 @@ const Layer = require('beet.js/lib/layer');
 
 document.querySelector('button').addEventListener('click', function() {
   const context = new AudioContext();
-
+  context.createOscillator();
   const beet = new Beet({
     context: context,
+    tempo: 120,
   });
 
-  let i60 = 0;
-  let i90 = 0;
-
-  beet.add(new Layer(context, 90, beet.pattern(4), (time, step, timeFromScheduled) => setTimeout(() => document.getElementById('bpm90').append(++i90 + ' '), timeFromScheduled * 1000)));
-  beet.add(new Layer(context, 60, beet.pattern(4), (time, step, timeFromScheduled) => setTimeout(() => document.getElementById('bpm60').append(++i60 + ' '), timeFromScheduled * 1000)));
-  beet.start();
+  beet.add(
+    new Layer(context, 90, beet.pattern(4), (time, step, timeFromScheduled) => setTimeout(() => document.getElementById('bpm90').append(step + ' '), timeFromScheduled * 1000)),
+    new Layer(context, 60, beet.pattern(4), (time, step, timeFromScheduled) => setTimeout(() => document.getElementById('bpm60').append(step + ' '), timeFromScheduled * 1000)),
+  ).start();
 });
 
 },{"beet.js":1,"beet.js/lib/layer":3}]},{},[13]);
