3e295cd276229ace5ca36b5de9fe62be94683bcd
jfw7 <jwall@peopledesigns.com>
2021-07-28 23:33:58 -0400

    make changes for audio playback, colors, 1 iteration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const Beet = require('beet.js');
 const Layer = require('beet.js/lib/layer');
 const Pattern = require('beet.js/lib/pattern');
 const d3 = require('d3');
+const Tone = require('tone');
 
 const width = 1000;
 const height = 800;
@@ -10,24 +11,30 @@ const radius = width / 2;
 const armRadius = radius / 22;
 const dotRadius = armRadius - 11;
 
-const color = d3.scaleSequential([0, 2 * Math.PI], d3.interpolateRainbow);
-
 const fields = [
   {
-    radius: 0.6 * radius,
+    color: '#B73225',
     pattern: '1000000100000010000001000000100000010000001000000100000010000000',
+    radius: 0.6 * radius,
+    sample: 'Snare-Dittie.wav',
   },
   {
-    radius: 0.45 * radius,
+    color: '#591C0B',
     pattern: 32,
+    radius: 0.45 * radius,
+    sample: 'Hat-Toefy.wav',
   },
   {
-    radius: 0.33 * radius,
+    color: '#004E7C',
     pattern: 16,
+    radius: 0.33 * radius,
+    sample: 'Perc-Koebal.wav',
   },
   {
-    radius: 0.2 * radius,
+    color: '#5C5F58',
     pattern: 8,
+    radius: 0.2 * radius,
+    sample: 'Perc-Bedouin.wav',
   },
 ].map(({ pattern, ...field}) => ({ ...field, pattern: new Pattern(pattern)}));
 
@@ -68,41 +75,62 @@ const fieldCircle = fieldTick.append('circle')
   .attr('r', dotRadius)
   .attr('fill', 'white')
   .attr('stroke', 'black')
-  .style('color', (d, i) => color(i / d.field.range.length * 2 * Math.PI))
+  .style('color', ({ field: { color } }) => color)
   .style('transition', 'fill 50ms ease-out');
 
-document.querySelector('button').addEventListener('click', function() {
+document.querySelector('button').addEventListener('click', async function() {
+  await Tone.start();
+
   const context = new AudioContext();
   context.createOscillator();
   const tempo = 37.5 / 2;
   const beet = new Beet({ context, tempo });
 
   field.each((d, i, nodes) => {
+    let started = false;
+    let stopped = false;
+    const player = new Tone.Player(`/audio/${d.sample}`).toDestination();
     const layer = beet.layer(
       d.pattern,
-      (time, step, timeFromScheduled) => setTimeout(
-        () => {
-          d3.select(nodes[i])
-            .selectAll('.field-circle')
-            .attr(
-              'fill',
-              (_, j) => {
-                return j === (step - 1) ? "currentColor" : "white";
-              },
-            );
-        },
-        timeFromScheduled * 1000,
-      ),
-      (time, step, timeFromScheduled) => setTimeout(
-        () => {
-          d3.select(nodes[i])
-            .selectAll('.field-circle')
-            .attr('fill', 'white');
-        },
-        timeFromScheduled * 1000,
-      ),
+      (time, step, timeFromScheduled) => {
+        setTimeout(
+          () => {
+            player.start();
+            d3.select(nodes[i])
+              .selectAll('.field-circle')
+              .attr(
+                'fill',
+                (_, j) => j === (step - 1) ? "currentColor" : "white",
+              );
+          },
+          timeFromScheduled * 1000,
+        );
+        if (step === 1) {
+          if (started) {
+            layer.tempo = 0;
+          }
+          started = true;
+        }
+      },
+      (time, step, timeFromScheduled) => {
+        setTimeout(
+          () => {
+            d3.select(nodes[i])
+              .selectAll('.field-circle')
+              .attr('fill', 'white');
+          },
+          timeFromScheduled * 1000,
+        );
+        if (step === 1) {
+          if (started) {
+            layer.tempo = 0;
+          }
+          started = true;
+        }
+      },
     );
     beet.add(layer);
   });
-  beet.start();
+  await Tone.loaded();
+  setTimeout(() => beet.start(), 500);
 });
